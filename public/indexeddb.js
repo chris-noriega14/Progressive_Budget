3e19faaac9d05c3a460785b52718d4050a0a3323
chris-noriega14 <chris.noriega14@hotmail.com>
2021-11-29 21:47:47 -0600

    Removed a link from files to cache, removed extra waitUntil function

--- public/indexeddb.js
+++ public/indexeddb.js
@@ -28,13 +28,13 @@ function checkDatabase() {
   // Open a transaction on the indexed db database "BudgetStore"
   let transaction = db.transaction(['BudgetStore'], 'readwrite');
 
-  // access your BudgetStore object
+  // access the BudgetStore object
   const store = transaction.objectStore('BudgetStore');
 
-  // Get all records from store using a created variable
+  // Get all records from store using a created variable "getAll"
   const getAll = store.getAll();
 
-  // If the request was successful
+  // If the request to get all records was successful, run the function
   getAll.onsuccess = function () {
     // If there are items in the store, we need to bulk add them when we are back online
     if (getAll.result.length > 0) {
@@ -58,7 +58,7 @@ function checkDatabase() {
 
             // Clear existing entries because our bulk add was successful
             currentStore.clear();
-            console.log('Clearing store 🧹');
+            console.log('Clearing store');
           }
         });
     }
@@ -71,7 +71,7 @@ request.onsuccess = function (e) {
 
   // Check if app is online before reading from db
   if (navigator.onLine) {
-    console.log('Backend online! 🗄️');
+    console.log('Backend is online!');
     checkDatabase();
   }
 };
@@ -81,10 +81,10 @@ const saveRecord = (record) => {
   // Create a transaction on the BudgetStore db with readwrite access
   const transaction = db.transaction(['BudgetStore'], 'readwrite');
 
-  // Access your BudgetStore object store
+  // Access the BudgetStore object store
   const store = transaction.objectStore('BudgetStore');
 
-  // Add record to your store with add method.
+  // Add record to your store using the add method.
   store.add(record);
 };
 
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,36 +1,32 @@
+const CACHE_NAME = "static-cache-v2"; //Static Data -files
+const DATA_CACHE_NAME = "data-cache-v1"; //Back-end data - api
+
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
-  "/service-worker.js",
   "/style.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
 
-const CACHE_NAME = "static-cache-v2"; //Static Data -files
-const DATA_CACHE_NAME = "data-cache-v1"; //Back-end data - api
-
 // install
-self.addEventListener("install", function (evt) {
-  // pre cache image data
-  evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-  );
-    
+self.addEventListener("install", (event) => {
+
   // pre cache all static files
-  evt.waitUntil(
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    .then(self.skipWaiting())
   );
 
   // tell the browser to activate this service worker immediately once it
   // has finished installing
-  self.skipWaiting();
+  
 });
 
 // activate
-self.addEventListener("activate", function(evt) {
-  evt.waitUntil(
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
@@ -47,22 +43,22 @@ self.addEventListener("activate", function(evt) {
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
+self.addEventListener("fetch", function(event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
+        return fetch(event.request)
           .then(response => {
             // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
+              cache.put(event.request.url, response.clone());
             }
 
             return response;
           })
           .catch(err => {
             // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
+            return cache.match(event.request);
           });
       }).catch(err => console.log(err))
     );
@@ -70,10 +66,10 @@ self.addEventListener("fetch", function(evt) {
     return;
   }
 
-  evt.respondWith(
+  event.respondWith(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.match(evt.request).then(response => {
-        return response || fetch(evt.request);
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request);
       });
     })
   );
