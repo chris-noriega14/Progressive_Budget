6fae231cb51dc8606bf9f664d50d3dc199436c81
chris-noriega14 <chris.noriega14@hotmail.com>
2021-11-30 00:12:52 -0600

    Fixed typo in service worker.js file styles.css

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const CACHEFILES = [
   '/',
   '/index.html',
-  '/style.css',
+  '/styles.css',
   '/index.js',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
